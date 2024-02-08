15ae399422703b2b9149dc63d5ad26fc33a84819
NobleMahmud <mahmud.oneis1@gmail.com>
2024-02-08 15:48:43 +0600

    cleaned 10, 12. added in 9

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port = process.env.PORT||5000;
 
 //  Middleware
 // app.get(cors()
+"this is a new line in line no 9 without conflict"
 
-"Thiis is a new line for testing purpose created by Nobel."
 
-"This is branch is created by Second branch and is absent in Nobel Branch"
+
 app.get('/',(req,res)=>{
       res.send('Happy Life is important to live in earth')
 })
